04c3b7bb38d3822f44bf95bd1961e1aa079b9c73
Titus Wormer <tituswormer@gmail.com>
2015-07-06 23:29:55 +0200

    Rebuild distribution

--- detab.js
+++ detab.js
@@ -1,7 +1,15 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.detab = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+/*
+ * Dependencies.
+ */
+
 var repeat = require('repeat-string');
+
+/*
+ * Constants.
+ */
 
 var TAB = '\t';
 var NEWLINE = '\n';
@@ -9,6 +17,11 @@ var NEWLINE = '\n';
 /**
  * Replace tabs with spaces, being smart about which
  * column the tab is at and which size should be used.
+ *
+ * @example
+ *   detab('\tfoo\nbar\tbaz'); // '    foo\nbar baz'
+ *   detab('\tfoo\nbar\tbaz', 2); // '  foo\nbar baz'
+ *   detab('\tfoo\nbar\tbaz', 8); // '        foo\nbar     baz'
  *
  * @param {string} value - Value with tabs.
  * @param {number?} [size=4] - Tab-size.
@@ -41,6 +54,10 @@ function detab(value, size) {
 
     return characters.join('');
 }
+
+/*
+ * Expose.
+ */
 
 module.exports = detab;
 
