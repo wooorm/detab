bfe2d9e5797785b5864bb3283ea87c0800f0e2f7
Titus Wormer <tituswormer@gmail.com>
2021-08-23 15:08:39 +0200

    Update dev-dependencies

--- test.js
+++ test.js
@@ -6,25 +6,25 @@ const own = {}.hasOwnProperty
 test('detab(value[, size])', (t) => {
   t.throws(
     () => {
-      // @ts-ignore runtime
+      // @ts-expect-error runtime
       detab(true)
     },
     /detab expected string/,
     'should throw when not given a string'
   )
 
-  t.test('should work', (st) => {
-    st.equal(detab('foo\tbar'), 'foo bar')
-    st.equal(detab('fo\tbar'), 'fo  bar')
-    st.equal(detab('f\tbar'), 'f   bar')
-    st.equal(detab('\tbar'), '    bar')
-    st.equal(detab('\t\tbar'), '        bar')
-    st.equal(detab('al\t\tbar'), 'al      bar')
-    st.equal(detab('bar\t\t'), 'bar     ')
-    st.end()
+  t.test('should work', (t) => {
+    t.equal(detab('foo\tbar'), 'foo bar')
+    t.equal(detab('fo\tbar'), 'fo  bar')
+    t.equal(detab('f\tbar'), 'f   bar')
+    t.equal(detab('\tbar'), '    bar')
+    t.equal(detab('\t\tbar'), '        bar')
+    t.equal(detab('al\t\tbar'), 'al      bar')
+    t.equal(detab('bar\t\t'), 'bar     ')
+    t.end()
   })
 
-  t.test('should support lines', (st) => {
+  t.test('should support lines', (t) => {
     const map = {
       LF: '\n',
       CR: '\r',
@@ -37,20 +37,20 @@ test('detab(value[, size])', (t) => {
       if (own.call(map, key)) {
         const chars = map[key]
 
-        st.test(key, (sst) => {
-          sst.equal(detab('foo' + chars + '\tbar'), 'foo' + chars + '    bar')
-          sst.equal(detab('fo' + chars + '\tbar'), 'fo' + chars + '    bar')
-          sst.equal(detab('f' + chars + '\tbar'), 'f' + chars + '    bar')
-          sst.equal(detab(chars + '\tbar'), chars + '    bar')
-          sst.equal(detab('al\t' + chars + '\tbar'), 'al  ' + chars + '    bar')
-          sst.equal(detab('bar' + chars + '\t'), 'bar' + chars + '    ')
-          sst.equal(detab('bar\t' + chars), 'bar ' + chars)
-          sst.end()
+        t.test(key, (t) => {
+          t.equal(detab('foo' + chars + '\tbar'), 'foo' + chars + '    bar')
+          t.equal(detab('fo' + chars + '\tbar'), 'fo' + chars + '    bar')
+          t.equal(detab('f' + chars + '\tbar'), 'f' + chars + '    bar')
+          t.equal(detab(chars + '\tbar'), chars + '    bar')
+          t.equal(detab('al\t' + chars + '\tbar'), 'al  ' + chars + '    bar')
+          t.equal(detab('bar' + chars + '\t'), 'bar' + chars + '    ')
+          t.equal(detab('bar\t' + chars), 'bar ' + chars)
+          t.end()
         })
       }
     }
 
-    st.end()
+    t.end()
   })
 
   t.end()
